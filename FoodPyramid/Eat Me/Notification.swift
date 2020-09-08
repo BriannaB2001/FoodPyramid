f22ce3b572bfdd9f57eede782710b041333a3197
Brianna <nanitooze@gmail.com>
2020-09-08 10:39:33 -0600

    notification

--- FoodPyramid/Eat Me/Notification.swift
+++ FoodPyramid/Eat Me/Notification.swift
@@ -12,72 +12,72 @@ import UserNotifications
 struct Notify {
     
     static func eatHealthyBreakfast() {
-    let center = UNUserNotificationCenter.current()
-
-    let content = UNMutableNotificationContent()
-    content.title = "Remember To Eat Healthy"
-    content.body = "Have some milk and fruit with your breakfast this morning! "
-    content.sound = UNNotificationSound.default
-
-    var dateComponents = DateComponents()
-    dateComponents.hour = 8
-    dateComponents.minute = 30
-    let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
-
-    let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
-    center.add(request)
+        let center = UNUserNotificationCenter.current()
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Remember To Eat Healthy"
+        content.body = "Have some milk and fruit with your breakfast this morning! "
+        content.sound = UNNotificationSound.default
+        
+        var dateComponents = DateComponents()
+        dateComponents.hour = 8
+        dateComponents.minute = 30
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+        
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        center.add(request)
     }
     
     static func eatHealthyLunch() {
-       let center = UNUserNotificationCenter.current()
-
-       let content = UNMutableNotificationContent()
-       content.title = "Remember To Eat Healthy"
-       content.body = "Add a vegetable and a fruit to your lunch! "
-       content.sound = UNNotificationSound.default
-
-       var dateComponents = DateComponents()
-       dateComponents.hour = 1
-       dateComponents.minute = 00
-       let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
-
-       let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
-       center.add(request)
-       }
+        let center = UNUserNotificationCenter.current()
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Remember To Eat Healthy"
+        content.body = "Add a vegetable and a fruit to your lunch! "
+        content.sound = UNNotificationSound.default
+        
+        var dateComponents = DateComponents()
+        dateComponents.hour = 1
+        dateComponents.minute = 00
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+        
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        center.add(request)
+    }
     
     static func eatHealthySnack() {
-       let center = UNUserNotificationCenter.current()
-
-       let content = UNMutableNotificationContent()
-       content.title = "Remember To Eat Healthy"
-       content.body = "Choose a healthier option for your snack! "
-       content.sound = UNNotificationSound.default
-
-       var dateComponents = DateComponents()
-       dateComponents.hour = 4
-       dateComponents.minute = 00
-       let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
-
-       let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
-       center.add(request)
-       }
-       
-       
+        let center = UNUserNotificationCenter.current()
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Remember To Eat Healthy"
+        content.body = "Choose a healthier option for your snack! "
+        content.sound = UNNotificationSound.default
+        
+        var dateComponents = DateComponents()
+        dateComponents.hour = 4
+        dateComponents.minute = 00
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+        
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        center.add(request)
+    }
+    
+    
     static func eatHealthyDinner() {
-       let center = UNUserNotificationCenter.current()
-
-       let content = UNMutableNotificationContent()
-       content.title = "Remember To Eat Healthy"
-       content.body = "Try to include all the food gorups in your dinner! "
-       content.sound = UNNotificationSound.default
-
-       var dateComponents = DateComponents()
-       dateComponents.hour = 6
-       dateComponents.minute = 30
-       let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
-
-       let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
-       center.add(request)
-       }
-       
+        let center = UNUserNotificationCenter.current()
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Remember To Eat Healthy"
+        content.body = "Try to include all the food gorups in your dinner! "
+        content.sound = UNNotificationSound.default
+        
+        var dateComponents = DateComponents()
+        dateComponents.hour = 6
+        dateComponents.minute = 30
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+        
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        center.add(request)
+    }
+    
 }
